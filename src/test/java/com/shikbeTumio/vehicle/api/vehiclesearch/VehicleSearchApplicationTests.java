940fd4e8ffaabee46155773a3c257494e7a5daea
Emon-Khan <rkemon94@gmail.com>
2023-10-26 09:15:21 +0600

    Intial setup of project

---
+++ src/test/java/com/shikbeTumio/vehicle/api/vehiclesearch/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikbeTumio.vehicle.api.vehiclesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
