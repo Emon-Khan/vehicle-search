21e555a18c1b39e3291af563683b279b069ec00a
Emon-Khan <rkemon94@gmail.com>
2023-10-29 11:49:49 +0600

    added rest endpoint to get all manufacturers

--- src/main/java/com/shikbeTumio/vehicle/api/vehiclesearch/controller/manufacturerController.java
+++ src/main/java/com/shikbeTumio/vehicle/api/vehiclesearch/controller/manufacturerController.java
@@ -5,10 +5,9 @@ import com.shikbeTumio.vehicle.api.vehiclesearch.service.ManufacturerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/manufacturers")
@@ -19,5 +18,10 @@ public class manufacturerController {
     public ResponseEntity<Manufacturer> createManufacturerInDB(@RequestBody Manufacturer manufacturer){
         Manufacturer dbRecord = manufacturerService.saveManufacturer(manufacturer);
         return new ResponseEntity<>(dbRecord, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Manufacturer>> getAllManufacturer(){
+        List<Manufacturer> allManufacturers = manufacturerService.fetchAllManufacturers();
+        return ResponseEntity.status(HttpStatus.OK).body(allManufacturers);
     }
 }
