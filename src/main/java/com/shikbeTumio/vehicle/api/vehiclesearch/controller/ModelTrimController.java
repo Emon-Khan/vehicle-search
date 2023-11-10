622edc39aafa9d29534743ca9c98977bbf290903
Emon-Khan <rkemon94@gmail.com>
2023-11-10 21:25:58 +0600

    Endpoint created for getting all vehicle model and it's corresponding trim_type and manufacturer with it

--- src/main/java/com/shikbeTumio/vehicle/api/vehiclesearch/controller/ModelTrimController.java
+++ src/main/java/com/shikbeTumio/vehicle/api/vehiclesearch/controller/ModelTrimController.java
@@ -4,24 +4,37 @@ import com.shikbeTumio.vehicle.api.vehiclesearch.entity.Model;
 import com.shikbeTumio.vehicle.api.vehiclesearch.entity.TrimType;
 import com.shikbeTumio.vehicle.api.vehiclesearch.service.ModelTrimService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/model-trim")
 public class ModelTrimController {
     @Autowired
     private ModelTrimService modelTrimService;
+
     @PostMapping
-    public ResponseEntity<Model> creatModelTrim(@RequestBody Model model){
-       Model saveRecord = modelTrimService.saveModel(model);
-       return new ResponseEntity<>(saveRecord, HttpStatus.CREATED);
+    public ResponseEntity<Model> creatModelTrim(@RequestBody Model model) {
+        Model saveRecord = modelTrimService.saveModel(model);
+        return new ResponseEntity<>(saveRecord, HttpStatus.CREATED);
     }
+
     @PostMapping("/trim-type")
-    public ResponseEntity<TrimType> createTrimType(@RequestBody TrimType trimType){
+    public ResponseEntity<TrimType> createTrimType(@RequestBody TrimType trimType) {
         TrimType saveTrimRecord = modelTrimService.saveTrimType(trimType);
         return new ResponseEntity<>(saveTrimRecord, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Model>> fetchAllModels() {
+        List<Model> dbModels = modelTrimService.getAllModels();
+        if (dbModels.size() > 0) {
+            return new ResponseEntity<>(dbModels, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
