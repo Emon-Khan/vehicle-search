58419554a0cf136fbf122390e0221757383a4914
Emon-Khan <rkemon94@gmail.com>
2023-12-11 10:53:22 +0600

    Authentication using jwt complete

---
+++ src/main/java/com/shikbeTumio/vehicle/api/vehiclesearch/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.shikbeTumio.vehicle.api.vehiclesearch.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
